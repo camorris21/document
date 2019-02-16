738209d20bf9a77505bb5319a1983a5f67d184e4
Chelsea Morris <chelsealaptop@mwc-114227.dhcp.missouri.edu>
2019-02-15 18:11:25 -0600

    adding the finished code

--- Document/Document/Program.cs
+++ Document/Document/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 
+using System.IO;
+
 namespace Document
 {
-    class Program
+
+    class Document
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+
+                Console.WriteLine("Document");
+
+                Console.WriteLine("Enter the Document Name:");
+
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter the Content of the Document:");
+
+                string content = Console.ReadLine();
+
+                string filename = name + ".txt";
+
+                string path = Environment.CurrentDirectory + "/" + filename;
+
+                if (!File.Exists(path))
+
+                {
+
+                    File.WriteAllText(path, content);
+
+                }
+
+                Console.WriteLine(filename + " was successfully saved. The document contains " + content.Length + " characters");
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                Console.WriteLine(e);
+
+            }
+
         }
+
     }
+
 }
